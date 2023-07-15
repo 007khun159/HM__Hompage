f7abcddf6a8f6e92dec1e4d7394886295d95a71d
007khun159 <008khun159@gmail.com>
2023-07-15 23:02:47 +0700

    Card-Layout

--- Fm_Homepage/src/App.jsx
+++ Fm_Homepage/src/App.jsx
@@ -2,7 +2,8 @@ import './styles/style.css'
 
 
 import Logo from './assets/images/logo.svg'
-
+import Desktop_Web from './assets/images/image-web-3-desktop.jpg'
+import Mobile_Web from './assets/images/image-web-3-mobile.jpg'
 
 function App() {
   
@@ -25,7 +26,50 @@ function App() {
             </div>
           </nav>
 
-              <section className="hero-section"></section>
+              <section className="hero-section">
+                      <div className="first-section">
+
+                        <div className="hero-img">
+
+                          <img src={Desktop_Web}/>
+                          <img src={Mobile_Web}/>
+
+                        </div>
+                      </div>
+                      <div className="intro">
+                          <h1>The Bright Future of Web 3.0?</h1>
+                          <div className="description">
+                              <p>
+                                We dive into the next evolution of the web that claims to put 
+                                the power of the platforms back into the hands of the people. 
+                                But is it really fulfilling its promise?
+                              </p>
+                              <button>Read more</button>
+                          </div>
+                      </div>
+                      <aside className='news'>
+                        <h2>New</h2>
+                        <div className="card">
+                          <h3></h3>
+                          <p></p>
+                        </div>
+                        <div className="card">
+                          <h3></h3>
+                          <p></p>
+                        </div>
+                        <div className="card">
+                          <h3></h3>
+                          <p></p>
+                        </div>
+                      </aside>
+                      <div className="second-section">
+
+                      </div>
+
+
+
+
+              </section>
 
         
         </div>
